bd190d353310d876b9a4ec86f9784073ac313c10
Greg <GregAStephen@gmail.com>
2018-11-27 20:47:37 -0600

    added functionality for the genre buttons. Still need to fix if no buttons are checked

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ let outputDiv = $("#output-records");
 let search = $("#search");
 let genreDiv = $("#genreOfMusicDiv");
 let moreOptionsButton = $("#moreOptionsButton");
+let searchForm = $("#recordSearchForm");
 
 function alphabetize() {
    recordCollection.sort(function(a, b){
@@ -21,61 +22,76 @@ function alphabetize() {
 function populateRecords(recordCollectionArray) { 
   let radioBtn = $("input[name=searchChoice]:checked").attr('id');
   let searchVal = search.val().toLowerCase();
+  let genreChoices = $("input:checkbox:checked").map(function(){
+    return $(this).attr('id');
+  }).get();
+  console.log("genre Choice:", genreChoices)
   ///// CLEARS THE DIV FOR THE NEW DISPLAY //////////////////
   outputDiv.html("");
+ 
+
   for(let i of recordCollectionArray) {
-    //CHECKS IF ARTIST RADIO BUTTON IS CHECKED///////////////
-    if (radioBtn === "artist") {
-      console.log(">>>radiobtn var >>>>>", radioBtn)
-       console.log("artist button works")
-       ////CHECKS IF THE VALUE IN THE SEARCH BAR MATCHES ANY ARTISTS//////
-      if (i.artist.toLowerCase().includes(searchVal)) { 
-       console.log(">>>radiobtn var >>>>>", radioBtn)
-        console.log("artist button works")
-       //SPITS OUT THE CARD USING THE INFORMATION FROM THE ARRAY/////////////////  
-        console.log((i.artist.toLowerCase().includes(searchVal)))
-        outputDiv.append(`<div class="card album bg-dark text-white col" style="width:18rem;"><h5 class="card-header">${i.artist}</h5><a target = "_blank" href="${i.link}"><img class="card-img-top location-image" src="${i.cover}"alt="${i.title}"></a><div class="card-footer">"${i.title}" -${i.year}</div></div>`).children(':last').hide().fadeIn(1500);
-        console.log(i.artist); 
-        console.log(search.val());
+    function outputResults() {
+      outputDiv.append(`<div class="card album bg-dark text-white col" style="width:18rem;"><h5 class="card-header">${i.artist}</h5><a target = "_blank" href="${i.link}"><img class="card-img-top location-image" src="${i.cover}"alt="${i.title}"></a><div class="card-footer">"${i.title}" -${i.year}</div></div>`).children(':last').hide().fadeIn(1500);
+    };
+    $.each(genreChoices, function(index, value){
+      console.log(value === i.genre.toLowerCase())
+        if (value === i.genre.toLowerCase()) {
+      //CHECKS IF ARTIST RADIO BUTTON IS CHECKED///////////////
+          if (radioBtn === "artist") {
+            console.log(">>>radiobtn var >>>>>", radioBtn)
+            console.log("artist button works")
+             ////CHECKS IF THE VALUE IN THE SEARCH BAR MATCHES ANY ARTISTS//////
+            if (i.artist.toLowerCase().includes(searchVal)) { 
+              console.log(">>>radiobtn var >>>>>", radioBtn)
+              console.log("artist button works")
+             //SPITS OUT THE CARD USING THE INFORMATION FROM THE ARRAY/////////////////  
+              console.log((i.artist.toLowerCase().includes(searchVal)))
+              outputResults();
+              console.log(i.artist); 
+              console.log(search.val());
             }
               ///CHECKS IF ALBUM RADIO IS CHECKED//////////////////
-      } else if(radioBtn === "album") {
-            ///THEN CHECKS IF VALUE IN SEARCH BAR MATCHES ANY ALBUM
-           if (i.title.toLowerCase().includes(searchVal)) {
-            // SPITS OUT A CARD USING THE INFORMATION FROM THE ARRAY
-              outputDiv.append(`<div class="card album bg-dark text-white col" style="width: 18rem;"><h5 class="card-header">${i.artist}</h5><a target = "_blank" href="${i.link}"><img class="card-img-top location-image" src="${i.cover}"alt="${i.title}"></a><div class="card-footer">"${i.title}" -${i.year}</div></div>`).children(':last').hide().fadeIn(1500);
-              console.log(i.title);
-              console.log(search.val());
-              console.log("album button works!")
+          } else if(radioBtn === "album") {
+              ///THEN CHECKS IF VALUE IN SEARCH BAR MATCHES ANY ALBUM
+              if (i.title.toLowerCase().includes(searchVal)) {
+                // SPITS OUT A CARD USING THE INFORMATION FROM THE ARRAY
+                outputResults();
+                console.log(i.title);
+                console.log(search.val());
+                console.log("album button works!")
             }
-             //// IF NEITHER BUTTON OR THE BOTH IS CLICKED THEN IT WILL SHOW EVERYTHING 
+          //// IF NEITHER BUTTON OR THE BOTH IS CLICKED THEN IT WILL SHOW EVERYTHING 
           } else {
-            console.log("both button works!")
-               if (i.title.toLowerCase().includes(searchVal) || i.artist.toLowerCase().includes(searchVal)) {
-                 outputDiv.append(`<div class="card album bg-dark text-white col" style="width: 18rem;"><h5 class="card-header">${i.artist}</h5><a target = "_blank" href="${i.link}"><img class="card-img-top location-image" src="${i.cover}"alt="${i.title}"></a><div class="card-footer">"${i.title}" -${i.year}</div></div>`).children(':last').hide().fadeIn(1500);
-              }          
-            }
-        
-   }   if(outputDiv.html() === "") {
-     //////IF THERE IS NOTHING SHOWN IN THE DIV THEN IT MEANS THE SEARCH RESULTED IN NOTHING AND SHOWS THIS MESSAGE//////
-        outputDiv.html("<h2>Sorry, there's nothing here for you at this time</h2>");
-       }
-     
+              console.log("both button works!")
+              if (i.title.toLowerCase().includes(searchVal) || i.artist.toLowerCase().includes(searchVal)) {
+                outputResults();
+            }          
+          }
+          //////IF THERE IS NOTHING SHOWN IN THE DIV THEN IT MEANS THE SEARCH RESULTED IN NOTHING AND SHOWS THIS MESSAGE//////
+        } 
+      }); 
+    } 
+      if(outputDiv.html() === "") {
+            outputDiv.html("<h2>Sorry, there's nothing here for you at this time</h2>");
+  }
 };
 
 
 genreDiv.hide();
 
-moreOptionsButton.click(function(e){
-  e.preventDefault();
+searchForm.on( "click", "#moreOptionsButton", function(event) {
+  event.preventDefault();
+  event.stopPropagation();
   genreDiv.show();
-  moreOptionsButton.hide();
+  $(this).hide();
 });
 
 
 
-searchBtn.click(function(e) {
-  e.preventDefault();
+searchForm.on( "click", "#see-records", function(event) {
+  event.preventDefault();
+  event.stopPropagation();
   alphabetize();
   populateRecords(recordCollection);
 });
